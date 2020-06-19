74228483a494c6d5bf82e54d88260285d9c41026
lookinside11 <jxaqt008@qq.com>
2020-06-19 22:32:58 +0800

    OK

---
+++ src/test/java/com/lig/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lig.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
