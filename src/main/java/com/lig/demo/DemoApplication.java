afadd7d097a554ea20bb5c6aab6bafcf2f06f8c5
lookinside11 <jxaqt008@qq.com>
2020-06-19 23:47:17 +0800

    OK

--- src/main/java/com/lig/demo/DemoApplication.java
+++ src/main/java/com/lig/demo/DemoApplication.java
@@ -1,13 +1,18 @@
 package com.lig.demo;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication   {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+            SpringApplication app=new SpringApplication(DemoApplication.class);
+            app.run(args);
     }
+
+
 
 }
