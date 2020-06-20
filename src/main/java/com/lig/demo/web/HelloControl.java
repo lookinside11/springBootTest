08865b359b5a33cbbc7830943c04997e319ed56e
lookinside11 <jxaqt008@qq.com>
2020-06-20 09:35:43 +0800

    OK

--- src/main/java/com/lig/demo/web/HelloControl.java
+++ src/main/java/com/lig/demo/web/HelloControl.java
@@ -1,5 +1,6 @@
 package com.lig.demo.web;
 
+import com.lig.demo.impl.UserImpl;
 import org.junit.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.Mapping;
@@ -11,10 +12,15 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class HelloControl {
     //
-      @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String  hello(String name)
-    {
-        return  "Hello 世界"+name;
-    }
+
+      @RequestMapping(name="/getUser", method= RequestMethod.POST)
+      public UserImpl getUser() {
+          UserImpl user=new UserImpl();
+          user.setName("⼩小明");
+          user.setAddress("海淀区");
+          return user;
+      }
+
+
 
 }
--- src/test/java/com/lig/demo/DemoApplicationTests.java
+++ src/test/java/com/lig/demo/DemoApplicationTests.java
@@ -2,13 +2,16 @@ package com.lig.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("i am a test");
-    }
+
+
+}
 
 }
